a11dd8b9b689c8777d8024926c4c9d2a28e254bb
JPGB <joao.galindo@hotmail.com.br>
2025-12-14 11:40:19 -0300

    teste github pages

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
       fallback: "404.html",
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : "clinicaBudinLandingPage",
+      base: process.argv.includes("dev") ? "" : "/clinicaBudinLandingPage",
     },
   },
 };
